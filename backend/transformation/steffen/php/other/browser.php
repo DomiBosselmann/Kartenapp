c03e81603025bdc5fa8fe4434a914ce55294eba1
Steff <schteffens@gmail.com>
2012-07-05 22:12:30 +0200

    Finale Anpassungen für den neuen Server

--- backend/transformation/steffen/php/other/browser.php
+++ backend/transformation/steffen/php/other/browser.php
@@ -2,7 +2,7 @@
 
 
 if (strrpos($_SERVER['HTTP_USER_AGENT'], "MSIE") > 0) {
-	echo '<image src="http://www.schteffens.de/ie.png" />';
+	echo '<image src="../../../../../images/ie.png" />';
 } else {
 	echo 'good boy!';
 }
--- backend/transformation/steffen/php/some_svg.php
+++ backend/transformation/steffen/php/some_svg.php
@@ -1,5 +1,5 @@
 <?php 
-$transform = 'http://www.schteffens.de/karte/php/transform.php?';
+$transform = 'http://dhbwweb.draco.uberspace.de/backend/transformation/steffen/php/transform.php?';
 
 foreach ($_GET as $key => $value) {
 	echo file_get_contents($transform . 'l=' . $value);

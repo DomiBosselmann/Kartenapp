b5e4fe8f07e524df5e9da8662f1ff52bfdd171c9
Steff <schteffens@gmail.com>
2012-07-24 19:04:18 +0200

    import, export, save refactored

--- backend/php/db.php
+++ backend/php/db.php
@@ -2,84 +2,93 @@
 
 if ($_POST['a']) {
 
-	$_db_host = "127.0.0.1:3306";
-	$_db_username = "dhbwweb";
-	$_db_passwort = "password";
-	$_db_datenbank = "database";
+	session_start();
 
-	SESSION_START();
-
-	# Datenbankverbindung herstellen
-	$link = mysql_connect($_db_host, $_db_username, $_db_passwort);
-
-	# Hat die Verbindung geklappt ?
-	if (!$link) {
-		die("Keine Datenbankverbindung moeglich: " . mysql_error());
-	}
-
-	# Verbindung zur richtigen Datenbank herstellen
-	$datenbank = mysql_select_db($_db_datenbank, $link);
-
-	if (!$datenbank) {
-		echo "Kann die Datenbank nicht benutzen: " . mysql_error();
-		mysql_close($link);        # Datenbank-Connection schliessen
-		exit;                    # Programm beenden !
-	}
-
-	if (!empty($_POST["submit"])) {
-		#UserDaten verbergen
-		$_username = mysql_real_escape_string($_POST["username"]);
-		$_passwort = mysql_real_escape_string($_POST["passwort"]);
-
-		# Befehl fuer die MySQL Datenbank
-	}
 	$action = $_POST['a'];
 
-	switch ($action) {
-		case "i":
-			{
-				// echo routes
-				$data = json_decode($_POST['places']);
-				$route_name = $data['name'];
+	$db_host = "127.0.0.1:3306";
+	$db_username = "dhbwweb";
+	$db_password = "password";
+	$db_database = "database";
 
-				$_query = "SELECT * FROM TROUTES WHERE CNAME='$_cname' OR CUSER='$_cuser'";
-				$_result = mysql_query($_query) or die ("Fehler: " . mysql_error());
+	if ($_SESSION['loggedin'] === true) {
 
-				while($row = mysql_fetch_array($result)) {
-					$arr = array("name"=>$row['CNAME'], "length"=>$row['CLENGTH'], "user"=>$row['CUSER'], "id"=>$row['CID']);
-					$json = json_encode($arr);
-					echo($json);
+		$link = mysql_connect($db_host, $db_username, $db_password);
+		if (!$link) {
+			echo_mysql_error($link, "Database connection error");
+		} else {
+
+			$database = mysql_select_db($db_database, $link);
+			if (!$database) {
+				echo_mysql_error($link, "Database selection error");
+			} else {
+
+				$username = $_SESSION['user'];
+
+				switch ($action) {
+					case "i":
+						{
+							// import routes as xml into the database and respond all available routes as json
+							$data = json_decode($_POST['data']);
+							// data parsing
+							$route_name = "";
+
+							$query = "INSERT INTO TROUTES (CNAME, CUSER) VALUES ('$route_name', '$username')";
+							$response = mysql_query($query);
+							if (!$response) {
+								echo_mysql_error($link, "Insertion error");
+							} else {
+
+								$query = "INSERT INTO TWAYPOINTS (CXKOORD, CYKOORD) VALUES ('$_cxkoord', '$_cykoord')";
+								$response = mysql_query($query);
+								if (!$response) {
+									echo_mysql_error($link, "Insertion error");
+								}
+							}
+							break;
+						}
+					case "e":
+						{
+							// export routes as xml
+
+							$query = "SELECT * FROM TROUTES WHERE CUSER='$username'";
+							$response = mysql_query($query);
+							if (!$response) {
+								echo_mysql_error($link, "Insertion error");
+							} else {
+
+								while($row = mysql_fetch_array($result)) {
+									$arr = array("name"=>$row['CNAME'], "length"=>$row['CLENGTH'], "user"=>$row['CUSER'], "id"=>$row['CID']);
+									$json = json_encode($arr);
+									echo($json);
+								}
+							}
+							break;
+						}
+					case "s":
+						{
+							// save routes into the database
+							$data = $_POST['data'];
+							// data parsing as json
+
+							// save parsed routes and nodes into the database
+
+							// echo success/fail
+							break;
+						}
 				}
-				getRoutes($_POST['data']);
-				break;
-			}
-		case "e":
-			{
-				// save routes
-				$_insert = "INSERT INTO TROUTES (CNAME, CLENGTH, CUSER) VALUES ('$_cName', '$_clength', '$_cuserName')";
-				$_query = mysql_query($insert);
 
-				if (!$_query) {
-					echo mysql_error($link);
-				}
-
-				$_insert = "INSERT INTO TWAYPOINTS (CXKOORD, CYKOORD) VALUES ('$_cxkoord', '$_cykoord')";
-				mysql_query($insert);
-				break;
+				mysql_close($link);
 			}
-		case "s":
-			{
-				break;
-			}
+		}
 	}
+}
 
-	if (!$_query) {
-		echo mysql_error($link);
+function echo_mysql_error($link, $error) {
+	if ($link) {
+		mysql_close($link);
 	}
-
-	# Datenbank-Connection wieder schliessen
-	mysql_close($link);
-
+	exit($error . ": " . mysql_error());
 }
 
 ?>

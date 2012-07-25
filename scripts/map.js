739f9ed09c663ee329648b726c25b705e357731c
Dominique Bosselmann <bosselmann@co2-filter.com>
2012-07-25 04:46:37 +0200

    Verbesserte Umrechnungsmethode von Pixel- und Geokoordinaten, die auch die Skalierung berücksichtigt

--- scripts/map.js
+++ scripts/map.js
@@ -1567,14 +1567,16 @@ window.Karte = (function () {
 			
 			return [x, y];
 		},
-		pixelCoordinateToGeoCoordinate : function (x, y, topLeft, topRight, witdh, height) {
-			var newX = (x + map.panning.x - map.dimensions.x) * map.scaling.scaleFactor; // Skalierung noch fehlerhaft!
-			var newY = (y + map.panning.y - map.dimensions.y) * map.scaling.scaleFactor;
-		
-			latitude = ((newY * (map.coordinates.bottomRight[0] - map.coordinates.topLeft[0])) / map.dimensions.currentHeight) + map.coordinates.topLeft[0];
-			longitude = ((newX * (map.coordinates.bottomRight[1] - map.coordinates.topLeft[1])) / map.dimensions.currentWidth) + map.coordinates.topLeft[1];
-			
-			return [latitude, longitude];
+		pixelCoordinateToGeoCoordinate : function (x, y) {			
+			var newX = (x - map.dimensions.x) / map.dimensions.currentWidth;
+            var longitudeDiff = map.coordinates.bottomRight[1] - map.coordinates.topLeft[1];
+            var longitude = map.coordinates.topLeft[1] + (longitudeDiff * newX);
+
+            var newY = (y - map.dimensions.y) / map.dimensions.currentHeight;
+            var latitudeDiff = map.coordinates.topLeft[0] - map.coordinates.bottomRight[0];
+            var latitude = map.coordinates.topLeft[0] - (latitudeDiff * newY);
+
+            return [latitude, longitude];			
 		}
 	};
 	
